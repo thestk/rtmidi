13bac1059a40a21f3443ff1e51566c7a50a26dc2
Rian Hunter <rian@alum.mit.edu>
2019-04-21 14:29:17 -0700

    Accept multiple MIDI messages in MidiOutAlsa::sendMessage
    Previously sendMessage() only accepted a single MIDI message
    at a time. This could cause extra latency due to invoking
    snd_seq_drain_output() (which invokes the write() system call)
    on every invocation. By allowing sendMessage() to accept
    multiple consecutive MIDI messages, this allows alsa seq
    to buffer multiple messages before invoking the write()
    system call, reducing latency per MIDI message.

--- RtMidi.cpp
+++ RtMidi.cpp
@@ -2293,7 +2293,7 @@ void MidiOutAlsa :: openVirtualPort( const std::string &portName )
 
 void MidiOutAlsa :: sendMessage( const unsigned char *message, size_t size )
 {
-  int result;
+  long result;
   AlsaMidiData *data = static_cast<AlsaMidiData *> (apiData_);
   unsigned int nBytes = static_cast<unsigned int> (size);
   if ( nBytes > data->bufferSize ) {
@@ -2313,25 +2313,38 @@ void MidiOutAlsa :: sendMessage( const unsigned char *message, size_t size )
     }
   }
 
-  snd_seq_event_t ev;
-  snd_seq_ev_clear( &ev );
-  snd_seq_ev_set_source( &ev, data->vport );
-  snd_seq_ev_set_subs( &ev );
-  snd_seq_ev_set_direct( &ev );
   for ( unsigned int i=0; i<nBytes; ++i ) data->buffer[i] = message[i];
-  result = snd_midi_event_encode( data->coder, data->buffer, (long)nBytes, &ev );
-  if ( result < (int)nBytes ) {
-    errorString_ = "MidiOutAlsa::sendMessage: event parsing error!";
-    error( RtMidiError::WARNING, errorString_ );
-    return;
-  }
 
-  // Send the event.
-  result = snd_seq_event_output( data->seq, &ev );
-  if ( result < 0 ) {
-    errorString_ = "MidiOutAlsa::sendMessage: error sending MIDI message to port.";
-    error( RtMidiError::WARNING, errorString_ );
-    return;
+  unsigned int offset = 0;
+  while (offset < nBytes) {
+    snd_seq_event_t ev;
+    snd_seq_ev_clear( &ev );
+    snd_seq_ev_set_source( &ev, data->vport );
+    snd_seq_ev_set_subs( &ev );
+    snd_seq_ev_set_direct( &ev );
+    result = snd_midi_event_encode( data->coder, data->buffer + offset,
+                                    (long)(nBytes - offset), &ev );
+    if ( result < 0 ) {
+      errorString_ = "MidiOutAlsa::sendMessage: event parsing error!";
+      error( RtMidiError::WARNING, errorString_ );
+      return;
+    }
+
+    if ( ev.type == SND_SEQ_EVENT_NONE ) {
+      errorString_ = "MidiOutAlsa::sendMessage: incomplete message!";
+      error( RtMidiError::WARNING, errorString_ );
+      return;
+    }
+
+    offset += result;
+
+    // Send the event.
+    result = snd_seq_event_output( data->seq, &ev );
+    if ( result < 0 ) {
+      errorString_ = "MidiOutAlsa::sendMessage: error sending MIDI message to port.";
+      error( RtMidiError::WARNING, errorString_ );
+      return;
+    }
   }
   snd_seq_drain_output( data->seq );
 }

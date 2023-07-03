33cc0443056dd21d88452913c9ab24a2272cf20b
Rylee Lyman <rylee.alanza@gmail.com>
2023-07-03 13:50:41 -0400

    fix: enable building on linux with alsa

--- build.zig
+++ build.zig
@@ -10,18 +10,34 @@ pub fn build(b: *Build) void {
         .target = target,
         .optimize = optimize,
     });
-    lib.addCSourceFiles(&.{
-        "RtMidi.cpp",
-        "rtmidi_c.cpp",
-    }, &.{
-        "-std=c++11",
-        "-D__MACOSX_CORE__",
-    });
+    const t = lib.target_info.target;
+    switch (t.os.tag) {
+        .macos => {
+            lib.addCSourceFiles(&.{
+                "RtMidi.cpp",
+                "rtmidi_c.cpp",
+                }, &.{
+                "-std=c++11",
+                "-D__MACOSX_CORE__",
+            });
+            lib.linkFramework("CoreServices");
+            lib.linkFramework("CoreAudio");
+            lib.linkFramework("CoreMIDI");
+            lib.linkFramework("CoreFoundation");
+        },
+        else => {
+            lib.addCSourceFiles(&.{
+                "RtMidi.cpp",
+                "rtmidi_c.cpp",
+                }, &.{
+                "-std=c++11",
+                "-D__LINUX_ALSA__",
+            });
+            lib.addIncludePath("/usr/include");
+            lib.linkSystemLibrary("asoundlib");
+        }
+    }
     lib.linkLibCpp();
-    lib.linkFramework("CoreServices");
-    lib.linkFramework("CoreAudio");
-    lib.linkFramework("CoreMIDI");
-    lib.linkFramework("CoreFoundation");
     lib.installHeader("rtmidi_c.h", "rtmidi/rtmidi_c.h");
 
     b.installArtifact(lib);
